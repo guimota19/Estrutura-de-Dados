6a50c6286e5d6e909166269ba4e52d1375a8cf28
GUILHERME DOS SANTOS MOTA <guilherme.mota@a.ucb.br>
2023-08-19 09:34:47 -0300

    atividade corrigida

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
  
- 
-int verificar_numero_primo (int n){
-return 0;
- 
-}
- 
 
 int main(int argc, char* argv[]) {
  
- int n, i, res=0;
+ int n, i, check = 0;
  
     n = atoi(argv[1]);
- 
-    for (i=2; i<=n/2;i++){
-        if (n % i == 0){
- 
-            res++;
+
+    printf("Numero inserido: %d",n);
+
+    for(i=2;i<n;i++){
+        if(n % i == 0){
+            check = 1;
             break;
         }
     }
- 
-    if (res == 0){
-        printf (" %d 0 - Numero nao primo.", n);
- 
+
+    if(check==0){
+        printf("\nO numero %d eh primo",n);
     } else {
-        printf (" %d 1 - Numero primo", n);
- 
+        printf("\nO numero %d nao eh primo",n);
     }
- 
+
+
+
 /*
     estrutura do argv serve como scanf mais simplificado, colocar depois do .exe
     sempre usar com #include <stdlib.h>
